106bc15eb37738fd3de65038291c50efe9e72f38
Jessica Cacau <jessicacacau@gmail.com>
2018-11-22 21:16:47 -0300

    Implementa getters

--- aresta.cpp
+++ aresta.cpp
@@ -1,7 +1,17 @@
 #include"aresta.hpp"
 
-Aresta::Aresta(int peso) {
+Aresta::Aresta(int v1, int v2, int peso) {
+    this->v1 = v1;
+    this->v2 = v2;
     this->peso = peso;
+}
+
+int Aresta::getV1() {
+    return v1;
+}
+
+int Aresta::getV2() {
+    return v2;
 }
 
 int Aresta::getPeso() {
