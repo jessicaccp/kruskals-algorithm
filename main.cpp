c3d63691cbcfe5b241477f0f827b31d2c92eb947
Jessica Cacau <jessicacacau@gmail.com>
2018-11-22 22:40:33 -0300

    Executa algoritmo de kruskal

--- main.cpp
+++ main.cpp
@@ -3,12 +3,14 @@
 #include"aresta.hpp"
 
 int main(int argc, char** argv) {
-    if (argc < 2)
+    if (argc < 2) {
         std::cout << "Para executar o programa, execute ./programa nome-do-arquivo.tsp" << std::endl;
-    else {
-        Grafo grafo(argv[1]);
-        grafo.criarGrafo();
+        exit(1);
     }
+
+    Grafo grafo(argv[1]);
+    grafo.criarGrafo();
+    grafo.kruskal();
 
     return 0;
 }
