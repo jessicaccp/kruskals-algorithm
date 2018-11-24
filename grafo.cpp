fe888bc1d8dbd40649c9c91235e3bb7d18073f3e
Jessica Cacau <jessicacacau@gmail.com>
2018-11-23 23:21:58 -0300

    Corrige indices e saida

--- grafo.cpp
+++ grafo.cpp
@@ -2,7 +2,7 @@
 #include<fstream>
 #include<string>
 #include<algorithm>
-#include<string.h>
+#include<cstring>
 #include"grafo.hpp"
 
 Grafo::Grafo(std::string arquivo) {
@@ -15,11 +15,12 @@ void Grafo::criarGrafo() {
     int inteiro;
 
     if (entrada.is_open()) {
+        std::cout << "Recebendo dados do arquivo..." << std::endl << std::endl;
+
         // Recebe nome
         entrada.ignore(100, ' ');
         entrada >> texto;
         setNome(texto);
-        std::cout << "Lendo nome..." << std::endl;
         
         // Recebe comentario
         entrada.ignore(100, '\n');
@@ -27,13 +28,11 @@ void Grafo::criarGrafo() {
         entrada.ignore(100, ' ');
         std::getline(entrada, texto);
         setComentario(texto);
-        std::cout << "Lendo comentario..." << std::endl;
 
         // Recebe dimensao
         entrada.ignore(100, ' ');
         entrada >> inteiro;
         setDimensao(inteiro);
-        std::cout << "Lendo dimensao..." << std::endl;
 
         // Recebe formato
         entrada.ignore(100, '\n');
@@ -41,17 +40,15 @@ void Grafo::criarGrafo() {
         entrada.ignore(100, ' ');
         entrada >> texto;
         setFormato(texto);
-        std::cout << "Lendo formato..." << std::endl;
 
         // Recebe pesos das arestas para cada tipo de matriz
         entrada.ignore(100, '\n');
         entrada.ignore(100, '\n');
-        std::cout << "Lendo pesos..." << std::endl;
 
         // Matriz superior sem diagonal principal
         if (getFormato() == "UPPER_ROW") {
-            for (int i = 1; i <= getDimensao(); i++) {
-                for (int j = i; j <= getDimensao(); j++) {
+            for (int i = 0; i < getDimensao(); i++) {
+                for (int j = i; j < getDimensao(); j++) {
                     if (i == j)
                         inteiro = 0;
                     else
@@ -67,27 +64,33 @@ void Grafo::criarGrafo() {
             for (int i = 1; i <= getDimensao(); i++) {
                 for (int j = 1; j <= i; j++) {
                     entrada >> inteiro;
-                    Aresta aresta(i, j, inteiro);
+                    Aresta aresta(i - 1, j - 1, inteiro);
                     arestas.push_back(aresta);
                 }
             }
         }
 
         else {
-            std::cout << "Formato nao identificado" << std::endl;
+            std::cout << "Formato nao identificado." << std::endl;
             exit(1);
         }
 
-        std::cout << "Grafo criado!" << std::endl;
+        std::cout   << "Nome: " << getNome() << std::endl
+                    << "Descricao: " << getComentario() << std::endl
+                    << "Formato da matriz: " << getFormato() << std::endl << std::endl;
+
+        std::cout << "Grafo criado." << std::endl << std::endl;
         entrada.close();
     }
     else {
-        std::cout << "Erro ao ler o arquivo" << std::endl;
+        std::cout << "Erro ao ler o arquivo." << std::endl;
         exit(1);
     }
 }
 
 void Grafo::kruskal() {
+    std::cout << "Executando o algoritmo de Kruskal..." << std::endl << std::endl;
+
     std::vector<Aresta> arvore;
     
     sort(arestas.begin(), arestas.end());
@@ -104,21 +107,25 @@ void Grafo::kruskal() {
         }
     }
 
+    std::cout << "AGM encontrada:" << std::endl << std::endl;
+
     for (int i = 0; i < arvore.size(); i++) {
-        std::cout   << "(" << arvore[i].getV1() << ", " << arvore[i].getV2()
+        std::cout   << "(" << arvore[i].getV1() + 1 << ", " << arvore[i].getV2() + 1
                     << ") = " << arvore[i].getPeso() << std::endl;
     }
 }
 
 int Grafo::buscar(int subset[], int i) {
-    if (subset[i] == -1)
-        return i;
-    return Grafo::buscar(subset, subset[i]);
+    if (subset[i] == -1) 
+		return i; 
+	return Grafo::buscar(subset, subset[i]); 
 }
 
 void Grafo::unir(int subset[], int v1, int v2) {
     int v1_set = buscar(subset, v1), v2_set = buscar(subset, v2);
-    subset[v1_set] = v2_set;
+
+    if (v1_set != v2_set)
+        subset[v1_set] = v2_set;
 }
 
 std::string Grafo::getFormato() {
@@ -127,6 +134,14 @@ std::string Grafo::getFormato() {
 
 int Grafo::getDimensao() {
     return dimensao;
+}
+
+std::string Grafo::getNome() {
+    return nome;
+}
+
+std::string Grafo::getComentario() {
+    return comentario;
 }
 
 void Grafo::setNome(std::string nome) {
